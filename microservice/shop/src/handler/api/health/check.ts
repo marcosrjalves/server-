b38e6a8ba3b3ebd2b02c34d86f69755ab749faf9
Marcos Alves <digosw@gmail.com>
2025-04-15 01:58:02 +0000

    ci: :rocket: testing auto deploy

--- microservice/shop/src/handler/api/health/check.ts
+++ microservice/shop/src/handler/api/health/check.ts
@@ -1,7 +1,8 @@
 /**
  * Health check
  */
-export const handler = async (): Promise<object> => {
+export const handler = async (event): Promise<object> => {
+  console.log('Health check event:', JSON.stringify(event, null, 2));
   return {
       statusCode: 200,
       body: JSON.stringify({
